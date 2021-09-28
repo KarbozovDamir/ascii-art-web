f9afaf3e03001ef7b671cb225061a4077af45cab
damirkap89 <damirkap89@gmail.com>
2021-09-28 21:53:20 +0600

    added handle and page of html css

--- main.go
+++ main.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	"fmt"
-	"log"
+
+	// "log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) { // обращаемся через w и выводим страницу. ResponseWriter - тип данных.
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+type User struct {
+}
+
+// func main() {
+// 	http.HandleFunc("/", handler)                // URL, method
+// 	log.Fatal(http.ListenAndServe(":8080", nil)) // обращение к методу - ListenAndServe. Создаем локальный сервер для отслеживания порта
+// }
+
+func home(w http.ResponseWriter, r *http.Request) { // обращаемся через w и выводим страницу. ResponseWriter - тип данных.
+	fmt.Fprintf(w, "Hi there!")
+}
+func contacts(w http.ResponseWriter, r *http.Request) { // обращаемся через w и выводим страницу. ResponseWriter - тип данных.
+	fmt.Fprintf(w, "contacts!")
+}
+
+func handleRequest() { // обращаемся через w и выводим страницу. ResponseWriter - тип данных.
+	http.HandleFunc("/", home)
+	http.HandleFunc("/contacts/", contacts)
+	http.ListenAndServe(":8080", nil)
+
 }
 
 func main() {
-	http.HandleFunc("/", handler)                // URL, method
-	log.Fatal(http.ListenAndServe(":8080", nil)) // обращение к методу - ListenAndServe. Создаем локальный сервер для отслеживания порта
+	handleRequest()
 }
